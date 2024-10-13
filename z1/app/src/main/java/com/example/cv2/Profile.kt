9e0f119e1a3a3a8a382edba95bb9b264ab8b5d53
Michal Vizváry <vizvarym1661@gmail.com>
2024-10-13 15:20:30 +0200

    feat: intro page, logout

--- z1/app/src/main/java/com/example/cv2/Profile.kt
+++ z1/app/src/main/java/com/example/cv2/Profile.kt
@@ -3,10 +3,15 @@ package com.example.cv2
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class Profile : Fragment(R.layout.fragment_profile) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        view.findViewById<Button>(R.id.logout).setOnClickListener { view ->
+            view.findNavController().navigate(R.id.logout)
+        }
     }
 }
