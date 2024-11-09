0e0b29502d1ea5292eab38abea9373feed125c1c
Michal Vizváry <vizvarym1661@gmail.com>
2024-11-09 16:25:44 +0100

    fix: feed adapter

--- z2/app/src/main/java/com/example/cv2/FeedAdapter.kt
+++ z2/app/src/main/java/com/example/cv2/FeedAdapter.kt
@@ -5,11 +5,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
-data class MyItem(val text: String)
+//data class MyItem(val text: String)
 
 class FeedAdapter : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
+    private var items: List<UserEntity> = listOf()
+
+    class FeedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.feed_item, parent, false)
+        return FeedViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FeedViewHolder, position: Int) {
+        holder.itemView.findViewById<TextView>(R.id.item_text).text = items[position].name
+    }
+
+    override fun getItemCount() = items.size
+
+    fun updateItems(newItems: List<UserEntity>) {
+        val diffCallback = ItemDiffCallback(items, newItems)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+
+        items = newItems
+        diffResult.dispatchUpdatesTo(this)
+    }
+/*
     private var items: List<MyItem> = listOf()
 
     class FeedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
@@ -29,5 +54,5 @@ class FeedAdapter : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
     fun updateItems(newItems: List<MyItem>) {
         items = newItems
         notifyDataSetChanged()
-    }
+    }*/
 }
