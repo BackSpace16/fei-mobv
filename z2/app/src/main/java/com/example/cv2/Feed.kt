3995298824a86be4935ef45f61501311889c61b8
Michal Vizváry <vizvarym1661@gmail.com>
2024-11-09 16:26:07 +0100

    feat: data binding in feed

--- z2/app/src/main/java/com/example/cv2/Feed.kt
+++ z2/app/src/main/java/com/example/cv2/Feed.kt
@@ -1,14 +1,59 @@
 package com.example.cv2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cv2.databinding.FragmentFeedBinding
 import com.google.android.material.snackbar.Snackbar
 
 class Feed : Fragment(R.layout.fragment_feed) {
+    private lateinit var viewModel: FeedViewModel
+    private var binding: FragmentFeedBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity(), object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return FeedViewModel(DataRepository.getInstance(requireContext())) as T
+            }
+        })[FeedViewModel::class.java]
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentFeedBinding.bind(view).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }.also { bnd ->
+            //bnd.bottomBar.setActive(BottomBar.FEED)
+
+            bnd.feedRecyclerview.layoutManager = LinearLayoutManager(context)
+            val feedAdapter = FeedAdapter()
+            bnd.feedRecyclerview.adapter = feedAdapter
+
+            // Pozorovanie zmeny hodnoty
+            viewModel.feedItems.observe(viewLifecycleOwner) { items ->
+                Log.d("FeedFragment", "nove hodnoty $items")
+                feedAdapter.updateItems(items.filterNotNull() ?: emptyList())
+            }
+
+            bnd.pullRefresh.setOnRefreshListener {
+                viewModel.updateItems()
+            }
+            viewModel.loading.observe(viewLifecycleOwner) {
+                bnd.pullRefresh.isRefreshing = it
+            }
+
+        }
+    }
+    /*
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -40,5 +85,5 @@ class Feed : Fragment(R.layout.fragment_feed) {
 
     private fun performAction() {
         Toast.makeText(context, "smola, skúste štastie nabudúce", Toast.LENGTH_SHORT).show()
-    }
+    }*/
 }
